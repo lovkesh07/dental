6a8a7595e42eed4988ceb27974bc8328e3edd1c8
lovkesh07 <79805511+lovkesh07@users.noreply.github.com>
2023-09-17 23:38:48 +0530

    sf

--- src/app/ContactUs/Contactform.jsx
+++ src/app/ContactUs/Contactform.jsx
@@ -36,7 +36,7 @@ const Contactform = () => {
   };
   return (
     <div>
-      <section class=" p-4 backdrop-filter  backdrop-blur-xl bg-opacity-20">
+      <section class=" p-4 backdrop-filter rounded-lg bg-orange-300 backdrop-blur-xl bg-opacity-20">
         <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
           <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-black">
             Contact Us
@@ -58,7 +58,7 @@ const Contactform = () => {
               <input
                 type="name"
                 id="name"
-                class="shadow-sm bg-gray-50 border border-blue-700 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-primary-500 block w-full p-2.5 dark:bg-blue-50 dark:border-gray-600 dark:placeholder-blue-300 dark:text-black dark:focus:ring-primary-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                class="shadow-sm bg-gray-50 border border-orange-700 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-primary-500 block w-full p-2.5 dark:bg-orange-50 dark:border-gray-600 dark:placeholder-orange-300 dark:text-black dark:focus:ring-primary-500 dark:focus:border-orange-500 dark:shadow-sm-light"
                 placeholder="Enter Your Name"
                 onChange={(e) => {
                                 setName(e.target.value);
@@ -77,7 +77,7 @@ const Contactform = () => {
               <input
                 type="Phoneno"
                 id="Phoneno"
-                class="shadow-sm bg-gray-50 border border-blue-700 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-primary-500 block w-full p-2.5 dark:bg-blue-50 dark:border-gray-600 dark:placeholder-blue-300 dark:text-black dark:focus:ring-primary-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                class="shadow-sm bg-gray-50 border border-orange-700 text-gray-900 text-sm rounded-lg focus:ring-orange-600 focus:border-primary-500 block w-full p-2.5 dark:bg-orange-50 dark:border-gray-600 dark:placeholder-orange-300 dark:text-black dark:focus:ring-primary-500 dark:focus:border-orange-500 dark:shadow-sm-light"
                 placeholder="Enter Your Phone No."
                 onChange={(e) => {
                                   setPhoneno(e.target.value);
@@ -96,7 +96,7 @@ const Contactform = () => {
               <input
                 type="email"
                 id="email"
-                class="shadow-sm bg-gray-50 border border-blue-700 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-primary-500 block w-full p-2.5 dark:bg-blue-50 dark:border-gray-600 dark:placeholder-blue-300 dark:text-black dark:focus:ring-primary-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                class="shadow-sm bg-gray-50 border border-orange-700 text-gray-900 text-sm rounded-lg focus:ring-orange-600 focus:border-primary-500 block w-full p-2.5 dark:bg-orange-50 dark:border-gray-600 dark:placeholder-orange-300 dark:text-black dark:focus:ring-primary-500 dark:focus:border-orange-500 dark:shadow-sm-light"
                 placeholder="Enter Your Email"
                 onChange={(e) => {
                                 setEmails(e.target.value);
@@ -115,7 +115,7 @@ const Contactform = () => {
               <input
                 type="text"
                 id="subject"
-                class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-blue-700 focus:border-primary-500 dark:bg-blue-50 dark:border-gray-600 dark:placeholder-blue-300 dark:text-black dark:focus:ring-primary-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-orange-700 focus:border-primary-500 dark:bg-orange-50 dark:border-gray-600 dark:placeholder-orange-300 dark:text-black dark:focus:ring-primary-500 dark:focus:border-orange-500 dark:shadow-sm-light"
                 placeholder="Let us know how we can help you ..."
                 onChange={(e) => {
                   setSubject(e.target.value);
@@ -133,7 +133,7 @@ const Contactform = () => {
               <textarea
                 id="message"
                 rows="6"
-                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-blue-700 focus:border-primary-500 dark:bg-blue-50 dark:border-gray-600 dark:placeholder-blue-300 dark:text-black dark:focus:ring-primary-500 dark:focus:border-blue-500"
+                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-orange-700 focus:border-primary-500 dark:bg-orange-50 dark:border-gray-600 dark:placeholder-orange-300 dark:text-black dark:focus:ring-primary-500 dark:focus:border-orange-500"
                 placeholder="Leave a comment..."
                 onChange={(e) => {
                                 setQuery(e.target.value);
@@ -142,7 +142,7 @@ const Contactform = () => {
             </div>
             <button
               type="submit"
-              class="my-6 px-12 w-[100%] border-blue-400 border-2 bg-blue-300 hover:bg-white text-blue-600 p-4 rounded-md"
+              class="my-6 px-12 w-[100%] border-red-400 border-2 bg-orange-300 hover:bg-white text-red-600 p-4 rounded-md"
             >
               Contact Us
             </button>

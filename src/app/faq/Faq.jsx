763a458eda62c6bb2842d44a44d2dae5d55f2f24
lovkesh patel <79805511+lovkesh07@users.noreply.github.com>
2024-12-11 22:16:29 +0530

    uio

--- src/app/faq/Faq.jsx
+++ src/app/faq/Faq.jsx
@@ -5,16 +5,16 @@ import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 const Faq = ({ open, toggle, title, desc }) => {
   return (
-    <div className="pt-[5px] w-[80%] mx-auto lg:p-[24] border-b-[1px] border-solid">
+    <div className="pt-[5px] lg:w-[80%] mx-auto lg:p-[24] border-b-[1px] border-solid">
       <div
-        className="hover:bg-orange-200 py-[10px] lg:px-[50px] flex justify-between items-center cursor-pointer "
+        className="hover:bg-orange-200 py-[10px] px-4 lg:px-[50px] flex justify-between items-center cursor-pointer "
         onClick={toggle}
       >
         <p className="lg:text-[20px] font-semibold " data-aos="fade-up">{title}</p>
         <div>{open ? <AiOutlineMinus className="lg:h-[40px] lg:w-[40px] font-bold" /> : <AiOutlinePlus className="lg:h-[40px] lg:w-[40px] font-bold" />}</div>
       </div>
       <Collapse isOpened={open}>
-        <div className=" lg:px-[50px] pb-[20px]" data-aos="fade-up">{desc}</div>
+        <div className="px-[15px] lg:px-[50px] pb-[20px]" data-aos="fade-up">{desc}</div>
       </Collapse>
     </div>
   );
